2c34457c311cfe6041ddacc17d4fcba232245e89
minhut <uongminh96@gmail.com>
2022-10-31 08:20:44 +0100

    Update string concat to stringBuffer, change some constant value

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -2,48 +2,52 @@ import java.text.NumberFormat;
 import java.util.*;
 
 public class StatementPrinter {
+  public final String TRAGEDY = "tragedy";
+  public final String COMEDY = "comedy";
 
   public String print(Invoice invoice, Map<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
-    String result = String.format("Statement for %s\n", invoice.customer);
+    StringBuilder result = Optional.ofNullable(String.format("Statement for %s\n", invoice.customer)).map(StringBuilder::new).orElse(null);
 
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+      int thisAmount;
 
       switch (play.type) {
-        case "tragedy":
+        case TRAGEDY -> {
           thisAmount = 40000;
           if (perf.audience > 30) {
             thisAmount += 1000 * (perf.audience - 30);
           }
-          break;
-        case "comedy":
+        }
+        case COMEDY -> {
           thisAmount = 30000;
           if (perf.audience > 20) {
             thisAmount += 10000 + 500 * (perf.audience - 20);
           }
           thisAmount += 300 * perf.audience;
-          break;
-        default:
+        }
+        default -> {
+          System.out.println("case default");
           throw new Error("unknown type: ${play.type}");
+        }
       }
 
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      if (COMEDY.equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+      result = (result == null ? new StringBuilder("null") : result).append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
       totalAmount += thisAmount;
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
+    result = (result == null ? new StringBuilder("null") : result).append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    result.append(String.format("You earned %s credits\n", volumeCredits));
+    return result.toString();
   }
 
 }
--- src/test/java/StatementPrinterTests.java
+++ src/test/java/StatementPrinterTests.java
@@ -1,3 +1,4 @@
+import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
